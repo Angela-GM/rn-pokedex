9f14f2e27a24dd430d44761b9a8ea4d888706767
Angela-GM <angelagm0112@gmail.com>
2023-09-04 11:47:49 +0200

    update

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,11 @@
 import {
+  StyleSheet,
   Text,
   SafeAreaView,
   FlatList,
   TouchableOpacity,
+  ImageBackground,
+  View,
 } from 'react-native';
 import { useContext } from 'react';
 import { AppContext } from '../context/Context';
@@ -14,38 +17,68 @@ import { useNavigation } from '@react-navigation/native';
 export default function HomeScreen() {
   const { state } = useContext(AppContext); // Obtén el estado de tu contexto global
   const navigation = useNavigation(); // Obtén la prop de navegación
+  
 
 
 
 
   return (
-    <SafeAreaView >
-    {state.pokemons.length === 0 ? (
-      <Text>Cargando...</Text>
-    ) : (
+    <ImageBackground
+      source={require("../../assets/pokemonBall.png")}
+      imageStyle={styles.backgroundImage}
+    >
+      <SafeAreaView>
+        <View>
+          <Text style={styles.title}>Pokedex</Text>
+        </View>
+        {state.pokemons.length === 0 ? (
+          <Text>Cargando...</Text>
+        ) : (
+          <FlatList
+            numColumns={2}
+            contentContainerStyle={{
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+            data={state.pokemons[0]}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('PokemonDetail', { url: item.url });
+                }}
+              >
+                <PokemonCard url={item && 'url' in item ? item.url : ''} />
+              </TouchableOpacity>
+            )}
 
-      // state.pokemons[0].map((item)=> item.name)
-      <FlatList
-        data={state.pokemons[0]}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-
-          <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('PokemonDetail', {url: item.url });
-
-          }}
-          >
-          <PokemonCard url={item && 'url' in item ? item.url : ''} />
-          </TouchableOpacity>
+          />
+          
           
         )}
-      />
-    )}
-  </SafeAreaView>
+        
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
-
+const styles = StyleSheet.create({
+  backgroundImage: {
+    opacity: 0.2,
+    width: 200,
+    height: 200,
+    transform: [{ rotate: "-20deg" }],
+    marginLeft: 270,
+    top: -50,
+  },
+  title: {
+    fontSize: 34,
+    fontWeight: "bold",
+    paddingHorizontal: 16,
+    color: "grey",
+    marginBottom: 5,
+    marginTop: 50,
+  },
+});
 
 
 
